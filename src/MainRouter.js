319fd0ada83328e2d0cd07a20664a175feed2d25
Pulkit Singh <pulkitsingh01@gmail.com>
2018-05-30 20:52:36 +0530

    Basic routing | ..

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Topics from './Routes/Topics/Topics.js'
+import Home from './Routes/Home/Home.js'
+import About from './Routes/About/About.js'
 
 const MainRouter = () => (
     <Router>
@@ -23,48 +26,6 @@ const MainRouter = () => (
         <Route path="/topics" component={Topics} />
       </div>
     </Router>
-  );
-  
-  const Home = () => (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-  
-  const About = () => (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-  
-  const Topics = ({ match }) => (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-  
-      <Route path={`${match.url}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.url}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-  
-  const Topic = ({ match }) => (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
+  );      
   
   export default MainRouter;
