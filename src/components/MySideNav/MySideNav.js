0041e7fd279d1f269e48961a78db5792cb5e6daf
Pulkit Singh <pulkitsingh01@gmail.com>
2018-06-17 21:25:29 +0530

    MySideNav | items

--- src/components/MySideNav/MySideNav.js
+++ src/components/MySideNav/MySideNav.js
@@ -16,58 +16,83 @@ class MySideNav extends Component {
     }
 
     onSubMenuClick(e) {
-        // var elems = e.currentTarget.querySelectorAll(".MySideNav-Item")
-        var SubMenuItems = $(e.currentTarget).find(".MySideNav-Item")
+        // var elems = e.currentTarget.querySelectorAll(".MySideNav-SubItem")
+        var SubMenuItems = $(e.currentTarget).find(".MySideNav-SubItem")
         if (SubMenuItems.length > 0) {
             SubMenuItems.each((index, item) => {
-                $(item).removeClass("MySideNav-Item").addClass("MySideNav-Item-Expanded");
+                $(item).removeClass("MySideNav-SubItem").addClass("MySideNav-SubItem-Expanded");
             });
         }
         else{
-            SubMenuItems = $(e.currentTarget).find(".MySideNav-Item-Expanded");
+            SubMenuItems = $(e.currentTarget).find(".MySideNav-SubItem-Expanded");
             SubMenuItems.each((index, item) => {
-                $(item).removeClass("MySideNav-Item-Expanded").addClass("MySideNav-Item");
+                $(item).removeClass("MySideNav-SubItem-Expanded").addClass("MySideNav-SubItem");
             });
         }
     }
 
+    getItems(){
+        var elems = []
+        this.props.items.forEach(element => {
+            elems.push(
+                <div className="MySideNav-Item">
+                    <NavLink to={"/"+element[1]} className="nav-text">{element[0]}</NavLink>
+                </div>
+            );
+        });
+        return elems;
+    }
+
     render() {
         var urlParts = window.location.href.split("/");
+        var items = this.getItems()
 
-        return <div className="MySideNav-Container">
-            <div className="MySideNav-Brand">
-                <NavLink to="/" className="nav-text">Vilokan Labs</NavLink>
-            </div>
-            <div className="MySideNav-Menu">
-                <NavLink to="/project" className="nav-text">Project</NavLink>
-            </div>
-            <div className="MySideNav-SubMenu" onClick={((e) => this.onSubMenuClick(e))}>
-                <NavLink to="/issues" className="nav-text">Issues</NavLink>
-                <div className="MySideNav-Item">
-                    <NavLink to="/issues" className="nav-text">List</NavLink>
-                </div>
-                <div className="MySideNav-Item">
-                    <NavLink to="/boards" className="nav-text">Boards</NavLink>
-                </div>
-                <div className="MySideNav-Item">
-                    <NavLink to="/labels" className="nav-text">Labels</NavLink>
-                </div>
-                <div className="MySideNav-Item">
-                    <NavLink to="/service_desk" className="nav-text">Service Desk</NavLink>
-                </div>
-                <div className="MySideNav-Item">
-                    <NavLink to="/milestones" className="nav-text">Milestones</NavLink>
+        return <div className="MySideNav-Wrapper">
+            <div className="MySideNav-Header">
+                <div className="MySideNav-Brand">
+                    <NavLink to="/" className="nav-text">Vilokan Labs</NavLink>
                 </div>
             </div>
-            <div className="MySideNav-Menu">
-                <NavLink to="/members" className="nav-text">Members</NavLink>
+            <div className="MySideNav-Container">
+                <div className="MySideNav-Item">
+                    <NavLink to="/project" className="nav-text">Project</NavLink>
+                </div>
+                <div className="MySideNav-SubMenu" onClick={((e) => this.onSubMenuClick(e))}>
+                    <NavLink to="/issues" className="nav-text">Issues</NavLink>
+                    <div className="MySideNav-SubItem">
+                        <NavLink to="/issues" className="nav-text">List</NavLink>
+                    </div>
+                    <div className="MySideNav-SubItem">
+                        <NavLink to="/boards" className="nav-text">Boards</NavLink>
+                    </div>
+                    <div className="MySideNav-SubItem">
+                        <NavLink to="/labels" className="nav-text">Labels</NavLink>
+                    </div>
+                    <div className="MySideNav-SubItem">
+                        <NavLink to="/service_desk" className="nav-text">Service Desk</NavLink>
+                    </div>
+                    <div className="MySideNav-SubItem">
+                        <NavLink to="/milestones" className="nav-text">Milestones</NavLink>
+                    </div>
+                </div>
+                <div className="MySideNav-Item">
+                    <NavLink to="/members" className="nav-text">Members</NavLink>
+                </div>
+                {items}
+            </div>
+            <div className="MySideNav-Footer">
+                {"<< Collapse Sidebar"}
             </div>
         </div>
     }
 }
 
-MySideNav.propTypes = {
-
+MySideNav.defaultProps = {
+    items:[
+        ["Project","project"],
+        ["Issues","issues"],
+        ["Members","members"],
+    ]
 };
 
 export default MySideNav
