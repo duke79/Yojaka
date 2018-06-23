b8bd10eb18da088c3545ab1a62860fb2295caf43
Pulkit Singh <pulkitsingh01@gmail.com>
2018-06-23 22:23:24 +0530

    Markdown | .

--- src/Routes/Issue/Issue.js
+++ src/Routes/Issue/Issue.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import MyBreadCrumb from '../../components/MyBreadCrumb/MyBreadCrumb.js'
 import IssueTitle from '../../components/IssueTitle/IssueTitle.js'
 import { NavLink } from "react-router-dom";
+import Markdown from '../../components/Markdown/Markdown'
 import './Issue.css'
 
 class Issue extends React.Component {
@@ -10,10 +11,52 @@ class Issue extends React.Component {
     return <div className="Issue-Wrapper">
       <MyBreadCrumb
         items={[
-        <NavLink to="/issues" className="nav-text">Issues</NavLink>,
-        <NavLink to="/issues/24" className="nav-text">#24</NavLink>
+          <NavLink to="/issues" className="nav-text">Issues</NavLink>,
+          <NavLink to="/issues/24" className="nav-text">#24</NavLink>
         ]} />
-      <IssueTitle/>
+      <Markdown
+        text="
+        # Dillinger
+
+        [![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
+
+        Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.
+
+        * Type some Markdown on the left
+        * See HTML in the right
+        * Magic
+
+        > The overriding design goal for Markdown's
+        > formatting syntax is to make it as readable
+        > as possible. The idea is that a
+
+
+        This text you see here is *actually* written in Markdown! To get a feel for Markdown's syntax, type some text into the left window and watch the results in the right.
+
+        ### Tech
+
+        Dillinger uses a number of open source projects to work properly:
+
+        * [AngularJS] - HTML enhanced for web apps!
+        * [Breakdance](http://breakdance.io) - HTML to Markdown converter
+        * [jQuery] - duh
+
+        And of course Dillinger itself is open source with a [public repository][dill]
+        on GitHub.
+
+        ### Installation
+
+        Dillinger requires [Node.js](https://nodejs.org/) v4+ to run.
+
+        Install the dependencies and devDependencies and start the server.
+        
+        For production environments...
+
+        ```sh
+        $ npm install --production
+        $ NODE_ENV=production node app
+        ```
+        " />
     </div>
   }
 }
