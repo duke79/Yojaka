4dac19980d1aa6b9f9e0056b94d85bcfaa9f5edb
Pulkit Singh <pulkitsingh01@gmail.com>
2018-06-26 22:17:50 +0530

    Routes | Issues | Container

--- src/Routes/Issues/Issues.js
+++ src/Routes/Issues/Issues.js
@@ -6,19 +6,62 @@ import MyCard from '../../components/MyCard/MyCard'
 import { NavLink } from "react-router-dom";
 import Menu from './Menu.js'
 import SearchBox from './SearchBox.js'
+import './Issues.css'
 
 
 class Issues extends React.Component {
   render() {
-    return <div>
+    return <div className="Issues-Wrapper">
       <MyBreadCrumb
         items={[<NavLink to="/issues" className="nav-text">Issues</NavLink>]} />
       <Menu />
       <SearchBox />
-      <IssueHeader/>
-      <MyCard/>
+      <div className="Issues-Container">
+        <div className="Issues-Container-Header">
+          <div className="Issues-Container-Header-States">
+            <div className="Issues-Container-Header-Open">{this.props.open_issues + " Open"}</div>
+            <div className="Issues-Container-Header-Close">{this.props.close_issues + " Closed"}</div>
+          </div>
+          <div className="Issues-Container-Header-Filters">
+            <div className="Issues-Container-Header-Filter">Author</div>
+          </div>
+        </div>
+        {this.props.issues.map((issue) =>
+          <div className="Issues-Container-Item">
+            <div className="Issues-Container-Item-Container">
+              <div className="Issues-Container-Item-Title">{issue.title}</div>
+              <div className="Issues-Container-Item-Info">
+                <div className="Issues-Container-Item-Info-Item">{"#" + issue.number}</div>
+                <div className="Issues-Container-Item-Info-Item">opened on</div>
+                <div className="Issues-Container-Item-Info-Item">{issue.date}</div>
+                <div className="Issues-Container-Item-Info-Item">by</div>
+                <div className="Issues-Container-Item-Info-Item">{issue.author}</div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   }
+}
+
+Issues.defaultProps = {
+  "open_issues": "6",
+  "close_issues": "3,592",
+  "issues": [
+    {
+      "title": "enabled word breaking of card title",
+      "number": "10485",
+      "date": "March 29",
+      "author": "bigbabla"
+    },
+    {
+      "title": "Another Issue",
+      "number": "445",
+      "date": "December 04",
+      "author": "bigbabla"
+    },
+  ]
 }
 
 export default Issues
