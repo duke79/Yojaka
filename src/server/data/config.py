e8e49c40a52d7b578faeba99dd542fa5f4a09813
Pulkit Singh <pulkitsingh01@gmail.com>
2018-07-28 09:26:13 +0530

    config | comments

--- src/server/data/config.py
+++ src/server/data/config.py
@@ -7,9 +7,13 @@ class ConfigError(Exception):
 
 
 class Config(dict):
-    def __init__(self, **kwargs):
-        dict.__init__(self, **kwargs)
+    def __str__(self):
+        return str(self.config)
 
+    def __init__(self):
+        dict.__init__(self, dict())  # Because dict is extended
+
+        # Open <user>/.yojaka/config.json
         user_home = os.path.expanduser("~")
         config_dir = os.path.join(user_home, ".yojaka")
         self.config_file = os.path.join(config_dir, "config.json")
@@ -17,25 +21,31 @@ class Config(dict):
         if not os.path.exists(config_dir):
             os.mkdir(config_dir)
 
+        # initialize self.config from config.json
         with open(self.config_file, "r+") as f:
             conf = f.read()
             if conf != "":
                 try:
                     self.config = json.loads(conf)
                 except json.decoder.JSONDecodeError as e:
-                    raise ConfigError("Config file corrupted")
+                    raise ConfigError("Config file invalid format")
             else:
                 self.config = dict()
 
-    def __str__(self):
-        return str(self.config)
+    def __enter__(self):
+        """ to enable 'with **' capability, counterpart function is __exit__ """
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """ commit at the end of 'with **' block """
+        self.commit()
 
     def __getitem__(self, key):
         # return super().__getitem__(key)
         try:
             return self.config[key]
         except KeyError as e:
-            self.__setitem__(key, dict())
+            self.__setitem__(key, dict())  # initialize the non-existent keys
             return self.config[key]
 
     def __setitem__(self, key, value):
@@ -43,16 +53,24 @@ class Config(dict):
         self.config[key] = value
 
     def commit(self):
-        ''' Commit the configuration changes to file '''
+        """
+        Commit the configuration changes to file
+        Use "with Config() as config" if auto commit is needed at the end,
+        otherwise use this method.
+        """
         with open(self.config_file, "w+") as f:
             json.dump(self.config, f, indent=4)
 
 
 if __name__ == "__main__":
-    config = Config()
-    print(config)
-    config["server"] = "http://127.0.0.1:5000"
-    database = {"type": "mysql"}
-    config["database"]["type"] = dict()
-    config["database"]["type"]["sdf"] = "mysqls"
-    config.commit()
+    with Config() as config:
+        print(config)
+        config["server"] = "http://127.0.0.1:5000"
+        config["database"] = {
+            "type": "mysql",
+            "user": "dbuser",
+            "host": "localhost",
+            "password": "secret-pass"
+        }
+
+        # config.commit()
