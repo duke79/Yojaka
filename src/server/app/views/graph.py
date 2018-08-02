b40f006a50e2e90b08bd4710f998192ba66279c6
duke79 <pulkitsingh01@gmail.com>
2018-08-02 21:39:42 +0530

    flask-graphql

---
+++ src/server/app/views/graph.py
@@ -0,0 +1,11 @@
+# https://github.com/graphql-python/flask-graphql
+
+from flask_graphql import GraphQLView
+from app.data.graph.query import schema
+
+from app import app
+
+app.add_url_rule('/graph', view_func=GraphQLView.as_view('graph', schema=schema, graphql=True))
+
+# Optional, for adding batch query support (used in Apollo-Client)
+# app.add_url_rule('/graphql/batch', view_func=GraphQLView.as_view('graphql', schema=schema, batch=True))
