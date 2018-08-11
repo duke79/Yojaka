4e588d7431075190389287d9b68a097b8998cdf3
duke79 <pulkitsingh01@gmail.com>
2018-08-11 21:57:54 +0530

    apollo | installed

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -5,19 +5,30 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes/Routes'
 
-
+/*Redux*/
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+/*Apollo*/
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-window.papa = {
-    title: "ReacTron",
-    home: "http://www.google.com/ncr"
-}
+const httpLink = createHttpLink({
+    uri: 'http://127.0.0.1:5000'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Routes />
-    </Provider>,
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <Routes />
+        </Provider>,
+    </ApolloProvider>,
     document.getElementById('root')
 );
 
