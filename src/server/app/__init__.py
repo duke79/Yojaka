27ec7c09c900f811e63e58b64cd214d1ca9dda71
duke79 <pulkitsingh01@gmail.com>
2018-10-31 20:56:16 +0530

    flask_migrate | pipenv run flask db init

--- src/server/app/__init__.py
+++ src/server/app/__init__.py
@@ -1,17 +1,25 @@
 # HTTP Error Codes and their meaning: https://www.symantec.com/connect/articles/http-error-codes
+import os
+
 from dukepy.config import Config
+user_home = os.path.expanduser("~")
+config_dir = os.path.join(user_home, ".yojaka")
+config_file = os.path.join(config_dir, "config.json")
+config = Config(path=config_file)
+
 from dukepy.sql_alchemy.tables.alchemy_base import db_uri, AlchemyBase
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 ## Init SQLAlchemy
-config = Config()
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 if config["debug"]:
     app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app, model_class=AlchemyBase)
+migrate = Migrate(app, db)
 
 from app.views import auth  # /api/auth
 from app.views import user  # /api/user
